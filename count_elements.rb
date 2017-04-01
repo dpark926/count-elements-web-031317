c66b8b62f4fb63f83a4c7b7070dba08ff9207a0c
Daniel Park <dpeace926@gmail.com>
2017-04-01 14:08:50 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
-def count_elements(array)
+require 'pry'
+def count_elements(array) #["cat", "dog", "fish", "fish"]
   # code goes here
+  # {'tortoise' => 3, 'aye-aye' => 2, 'honey badger' => 1}
+  hash = {}
+
+  array.each do |word|
+    if hash[word]
+      hash[word] += 1
+    else
+      hash[word] = 1
+    end
+  end
+
+  hash
 end
- 
